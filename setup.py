cedd1d0f561369a39d4aa819747e45f60c3fafda
Phil Wang <lucidrains@gmail.com>
2020-11-07 11:31:19 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'saoa_pytorch',
+  packages = find_packages(exclude=['examples']),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Self Attention on Attention - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/SAoA-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'attention mechanism',
+    'visual question answering'
+  ],
+  install_requires=[
+    'torch>=1.6',
+    'einops>=0.3'
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
